55218d1d21fff00dea9c082c9cff3d75ddb0febd
Bruna Mortari <104204168+bruumortari@users.noreply.github.com>
2024-04-22 10:58:27 -0300

    Update index.tsx

--- Projeto Web/my-app/src/components/Header/index.tsx
+++ Projeto Web/my-app/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
-import { Container, Content, Content2, Content3 } from './styles'
+import { Container, Content, Content2 } from './styles'
 
 interface HeaderProps {
     onOpenUserModal: () => void;
@@ -12,22 +12,20 @@ export function Header({ onOpenUserModal }: HeaderProps) {
     return (
         <Container>
             <Content>
-                <img src={logoImg} alt="biblio" />
-                <span className='title'>biblio</span>
                 <button className='minhaConta' type="button" onClick={onOpenUserModal}>
                     Minha conta
                 </button>
-            </Content>
-            <Content2>
                 <button className='recs' type="button">
                     Recomendações
                 </button>
-            </Content2>
-            <Content3>
                 <button className='minhaEstante' type="button">
                     Minha estante
                 </button>
-            </Content3>
+                </Content>
+                <Content2>
+                <img src={logoImg} alt="biblio" />
+                <span className='title'>biblio</span>
+                </Content2>
         </Container>
     )
 }
